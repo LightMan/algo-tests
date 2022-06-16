4b0a5310b23b09a7f205b38800a32c2614d78b4b
David Jorge <david.jorge@appconcept.es>
2022-06-16 16:36:52 +0200

    add instructions

--- stairs.js
+++ stairs.js
@@ -1,5 +1,13 @@
 "use strict";
 
+// There is a start with a number n of steps
+// we can go upstairs by jump from one step at a time
+// two steps or three steps
+// but we have to finish in the last step exactly
+// Find the number of combinations to go upstairs
+// For example a 3 stepped stairs will have 4 combinations:
+// 1st=1+1+1 2nd=2+1 3rd=1+2 4=3
+
 const solution = (n) => {
   // Check minimum cases
   const steps = [1];
